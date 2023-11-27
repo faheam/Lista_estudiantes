db14e5c1f11d607046d0a301477758936909c7fb
faheam <151974229+faheam@users.noreply.github.com>
2023-11-27 15:34:37 -0300

    Add files via upload

---
+++ lista_estudiantes.js
@@ -0,0 +1,7 @@
+const mostrarListaEstudiantes = (estudiantes) => {
+    console.log("Lista de estudiantes registrados: ");
+    estudiantes.forEach((estudiante) => {
+        console.log(`Nombre: ${estudiante.nombre}, Edad: ${estudiante.edad}`);
+    });
+}
+module.exports = {mostrarListaEstudiantes}
